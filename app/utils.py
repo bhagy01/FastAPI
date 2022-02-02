6580ce6ea488fb594686ade8537e6b7137603960
Bhagiyaraj Ganesan <bhag3747@plintron.com>
2022-02-02 21:46:33 +0530

    added 2FA-Functionality

--- app/utils.py
+++ app/utils.py
@@ -1,18 +1,28 @@
 from datetime import datetime
 from passlib.context import CryptContext
-from pyotp import pyotp
-# print(pyotp.random_base32())
-#added TOTP-2FA functionality
-# mytimezone= Time Zone: (UTC+05:30)
+import pyotp
+import qrcode
 TOTP_KEY='ZZXL7ZVUETHFG7NNDDOJAEIBNAW75QJ7'
+
+# print(pyotp.random_base32())
+#added TOTP-2FA functpionality
+# mytimezone= Time Zone: (UTC+05:30)
+
 # totp_uri='otpauth://totp/FastAPI%20App:bhagiyarajmahesh1%40gmail.com?secret=ZZXL7ZVUETHFG7NNDDOJAEIBNAW75QJ7&issuer=FastAPI%20App'
 # otp = pyotp.parse_uri(totp_uri)
 # print(otp.now())
 totp = pyotp.TOTP(TOTP_KEY)
 print(totp.now())
-URI=pyotp.totp.TOTP(TOTP_KEY).provisioning_uri(
-name='bhagiyarajmahesh1@gmail.com', issuer_name='FastAPI App')
-# print(URI)
+y = TOTP_KEY
+with open("otp-sekrit.txt", "a") as f: 
+    f.write(y) 
+
+print(f"SAVE THIS : {y}")
+
+x = pyotp.totp.TOTP(y).provisioning_uri(name="FastAPI", issuer_name="FastAPI") 
+
+img = qrcode.make(x) 
+img.save("nerd.png")
 
 
 pwd_context = CryptContext(schemes=["bcrypt"],deprecated = "auto")
