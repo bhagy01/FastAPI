7380dc50a08c6fd5b6627d421e6bc7d40c49787f
Bhagiyaraj Ganesan <bhag3747@plintron.com>
2022-02-07 19:05:22 +0530

    Added 2FA for login route

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,18 +8,19 @@ from .. import models,schemas,utils,oauth
 router=APIRouter(tags=['Login'])  
 
 @router.post("/login",response_model=schemas.Token)
-def login_user(user_credentials:dict,db: Session = Depends(get_db)):
-   user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+def login_user(user_credentials:schemas.LoginUser,db: Session = Depends(get_db)):
+   user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
    if not user:
        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail= f"Invalid credentials")
+   if user.is_active is False:
+       raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="User is not active")
    if not utils.verify(user_credentials.password,user.password):
        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail= f"Invalid credentials")
-   otp_verify=utils.verify(user_credentials.otp)
+   otp_verify=utils.verify_totp(user_credentials.otp)
    if otp_verify is False:
-       raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail= f"Invalid credentials")
+       raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail= f"Invalid TOTP")
    access_token = oauth.create_access_token(data = {"user_id": user.id})
    return access_token
- 
  
 
 
